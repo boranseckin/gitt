c5f03883f6b1ffa2f7a0762d4bba26b5f1bc9e6e
Boran Seckin <boran@boranseckin.com>
2024-03-10 23:13:01 -0400

    feat: improve cat-file output

--- src/commands/cat_file.rs
+++ src/commands/cat_file.rs
@@ -1,17 +1,24 @@
+use std::io::{self, Write};
+
 use anyhow::Context;
 
-use crate::object::Object;
+use crate::object::{Kind, Object};
 
 pub(crate) fn invoke(pretty_print: bool, object_hash: &str) -> anyhow::Result<()> {
     anyhow::ensure!(
         pretty_print,
-        "object kind must be provided, but it's not implemented yet"
+        "object kind must be provided, but it's not implemented yet. Use -p option instead."
     );
 
     let object = Object::read(object_hash).context("failed to read object")?;
-    let hash = std::str::from_utf8(&object.content)?;
 
-    print!("{hash}");
+    if object.kind != Kind::Blob {
+        anyhow::bail!("object type {:?} is not ye implemented", object.kind);
+    }
+
+    io::stdout()
+        .write_all(&object.content)
+        .context("failed to write to stdout")?;
 
     Ok(())
 }
